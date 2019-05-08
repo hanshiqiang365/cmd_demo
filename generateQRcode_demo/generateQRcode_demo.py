8798b06ddbb438ba020df68bad330eaca7410b8c
hanshiqiang365 <shiqiang.han@accenture.com>
2019-05-09 00:03:55 +0800

    Add files via upload

---
+++ generateQRcode_demo/generateQRcode_demo.py
@@ -0,0 +1,17 @@
+﻿#author: hanshiqiang365 （微信公众号：韩思工作室）
+
+from MyQR import myqr
+import os
+words='http://weixin.qq.com/r/DkyxqYPE_96ErcfK9xkc'
+version, level, qr_name = myqr.run(
+    words,
+    version=1,
+    level='H',
+    picture="reversed_Ironman.gif",
+    colorized=True,
+    contrast=1.0,
+    brightness=1.0,
+    save_name=None,
+    save_dir=os.getcwd()
+)
+
---
+++ generateQRcode_demo/generateQRcode_standard_demo.py
@@ -0,0 +1,10 @@
+﻿#author: hanshiqiang365 （微信公众号：韩思工作室）
+
+import qrcode
+
+words='http://weixin.qq.com/r/DkyxqYPE_96ErcfK9xkc'
+
+img = qrcode.make(words)
+img.save('qrcode.png')
+img.show()
+
